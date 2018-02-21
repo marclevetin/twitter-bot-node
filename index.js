bd8ac17e0af7e7eb3e4ad1b4f6387d60d52b97b3
marclevetin <marc@derbyhat.org>
2018-02-20 23:14:30 -0500

    added randomized tweet content

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config();
 const Twitter = require('twitter');
+const moment = require('moment');
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -7,34 +8,28 @@ const client = new Twitter({
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
-
+debugger;
 const params = {screen_name: '_derbyhat'};
 
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-//     console.log(tweets);
-//   }
-// });
 const options = ["hello", "joke", "time"];
-
-const randomNumber = Math.floor(Math.random()*options.length);
+const randomNumber = Math.floor(Math.random() * options.length);
 const currentChoice = options[randomNumber];
 
 let statusMessage = '';
 
 if (currentChoice === 'hello') {
-  statusMessage = 'hi'
+  statusMessage = 'hi.  I wrote a bot that randomly posted this message.';
 } else if (currentChoice === 'joke') {
-  statusMessage = "Q: What do you call a cow with no legs? \n A: Ground Beef"
+  statusMessage = "Q: What do you call a cow with no legs? \n A: Ground Beef";
 } else if (currentChoice === 'time') {
-  statusMessage = 'Hickory, dickory, dock.  The mouse ran up the clock.'
+  const now = moment().format("h:mm:ss a");
+  statusMessage = 'Hickory, dickory, dock.  The mouse ran up the clock.  The time is ' + now;
 }
 
-
-
-client.post('statuses/update', {status: 'hi'})
+post to Twitter
+client.post('statuses/update', {status: statusMessage})
   .then(function (tweet) {
-    console.log(tweet);
+    console.log('tweet sent');
   })
   .catch(function (error) {
     throw error;
